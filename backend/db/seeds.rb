c7eca9708bc515f67557a0d77df35db935af9769
vvani <vvani06@gmail.com>
2020-04-25 18:46:43 +0900

    implement model: schedule

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Schedule.create(
+  name: "test_01",
+  run_at: Time.new(2000, 01, 01, 19, 0, 0, '+09:00'),
+  calc_index: 12
+)
